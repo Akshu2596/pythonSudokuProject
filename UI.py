21c02078bd4eed5a2845ebe88cc7966a46df9b98
Chef2596 <akankshaverma005@gmail.com>
2020-10-01 19:10:18 +0530

    #implemented checks for small 3*3 grid

--- UI.py
+++ UI.py
@@ -71,7 +71,9 @@ class App:
             if self.allCellsDone():
                 # check if all cells are done correctly
                 self.checkAllCells()
-                print(self.incorrectCells)
+                #print(self.incorrectCells)
+                if len(self.incorrectCells) == 0:
+                    print("Congratulations!")
 
     def playing_draw(self):
         self.window.fill(PINK)
@@ -101,9 +103,9 @@ class App:
         return True
 
     def checkAllCells(self):
-        self.checkRows()
-        #self.checkColumns()
-        #self.checkGrid()
+       self.checkRows()
+       self.checkColumns()
+       self.checkSmallGrid()
 
 
     def checkRows(self):
@@ -115,8 +117,39 @@ class App:
                     possibles.remove(self.grid[yidx][xidx])
 
                 else:
-                    if [xidx,yidx] not in self.lockedCells:
+                    if [xidx,yidx] not in self.lockedCells and [xidx, yidx] not in self.incorrectCells:
                         self.incorrectCells.append([xidx,yidx])
+
+    def checkColumns(self):
+        for xidx in range(9):
+
+            possibles = [1,2,3,4,5,6,7,8,9]
+            for yidx, row in enumerate(self.grid):
+
+                if self.grid[yidx][xidx] in possibles:
+                    possibles.remove(self.grid[yidx][xidx])
+
+                else:
+                    if [xidx,yidx] not in self.lockedCells and [xidx, yidx] not in self.incorrectCells:
+                        self.incorrectCells.append([xidx,yidx])
+
+    def checkSmallGrid(self):
+        for x in range(3):
+            for y in range(3):
+                #print(x,y)
+                possibles = [1,2,3,4,5,6,7,8,9]
+                #print("re setting possibles")
+                for i in range(3):
+                    for j in range(3):
+                        #print(x*3+i,y*3+j)
+                        xidx= x*3+i
+                        yidx =y*3+j
+                        if self.grid[yidx][xidx] in possibles:
+                            possibles.remove(self.grid[yidx][xidx])
+                        else:
+                            if [xidx, yidx] not in self.incorrectCells and [xidx,yidx] not in self.lockedCells:
+                                self.incorrectCells.append([xidx, yidx])
+                                #print("checked by smallGrid")
 
 
 
